bbb5cfd5f30248b9a8c5daae33505b22c7cc6df0
Haruka Yamashita <hyamashita@nig.ac.jp>
2019-08-28 19:08:10 +0900

    Remove details in setup(). Changed to read from setup.cfg file.

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="conspospy",
-    version="0.1.0",
-    author="Division of Evolutionary Genetics in NIG",
-    author_email="hyamashita@nig.ac.jp",
-    description="Python version of conspos package.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/yamasampo/conspospy",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'numpy'
-    ],
-)
+setup()
